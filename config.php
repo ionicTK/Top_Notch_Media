fab242d3ff25b12421910c91d53e89d61fe7f32b
Tanaka Madyara <tanakamadyara@gmail.com>
2020-10-31 16:46:20 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,6 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', '159.89.81.109');
-define('DB_USERNAME', 'octane');
-define('DB_PASSWORD', 'Zimbo_29!');
-define('DB_NAME', 'newMail');
+
  
 /* Attempt to connect to database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
